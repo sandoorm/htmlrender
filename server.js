2741c06eafa119b71b775733aaa35d60375386ed
sandoorm <s.maraczy@gmail.com>
2020-12-27 10:33:14 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 'use strict';
 
-const MY_TABLE_HTML = require( __dirname + "/table.html");
 const express = require('express');
 const puppeteer = require('puppeteer');
 const bodyParser = require('body-parser');
-const fs = require('fs');
 
 // Constants
 const PORT = 2078;
@@ -23,6 +21,7 @@ app.use(bodyParser.urlencoded({
 	extended: true,
 	parameterLimit: 50000
 }));
+
 
 app.use(function (req, res, next) {
 	if (req.is('text/*')) {
@@ -112,11 +111,11 @@ function render(req, res) {
 	if (req.is('application/json')) {
 		// If you are using a header and/or footer, then you probably want to specify
 		// top and bottom margin
-		headerHtml = MY_TABLE_HTML.header;
-		bodyHtml = MY_TABLE_HTML.body;
-		footerHtml = MY_TABLE_HTML.footer;
+		headerHtml = req.body.header;
+		bodyHtml = req.body.body;
+		footerHtml = req.body.footer;
 	} else if (req.text !== undefined) {
-		bodyHtml = MY_TABLE_HTML.body;
+		bodyHtml = req.text;
 	} else {
 		res.status(400).send('The body must have Content-Type:text/html');
 		return;
